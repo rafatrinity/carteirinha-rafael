365928235bfc8a0f47b6f6f421c7e90a7c387f77
RAFAEL TRINDADE <rafaeltrindade649@gmail.com>
2023-05-21 04:47:36 -0300

    Add files via upload

---
+++ view.js
@@ -0,0 +1,18 @@
+function openFullscreen() {
+  var elem = document.documentElement;
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+document.addEventListener("DOMContentLoaded", function () {
+  openFullscreen();
+});
